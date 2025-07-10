f03cd368b617d913f490dfeee139b3b462abb6a1
Adetutu234 <apanisilemorenikeji@gmail.com>
2025-07-10 23:33:36 +0100

    fixed an error in the server.js

--- Server.js
+++ Server.js
@@ -7,12 +7,11 @@ const app = express()
 const PORT = process.env.PORT
 const MY_DATABASE = process.env.MY_DATABASE
 
-app.use(express.json())
+app.use(express.json()) 
 
 mongoose.connect(MY_DATABASE)
-.then(()=>{console.log("Mongoose connected success")
+.then(()=>{console.log("Mongoose connected successfully")})
 .catch((error)=>{console.error("Mongoose connection failed")})
-})
 
 app.listen(PORT, ()=>{
     console.log(`Server is running on http://localhost: ${PORT}`);
